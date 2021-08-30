639735ecc6dd45c310539f1c579a55115be292e4
john gill <johnagill95@gmail.com>
2021-08-31 00:06:20 +0100

    remove all code

--- src/user/getUser.ts
+++
@@ -1,9 +0,0 @@
-export default async (db: any, id: string) => {
-  const query = 'SELECT * FROM public.users WHERE uid=$1';
-  try {
-    const data = await db.any(query, id);
-    return data;
-  } catch (error) {
-    console.log(error.message);
-  }
-};
